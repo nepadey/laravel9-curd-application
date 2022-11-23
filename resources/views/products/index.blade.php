48a51efe42a394d58a07cf78460a6fef661c695d
Surendra <surendragiri10@gmail.com>
2022-11-23 21:26:52 +0545

    From Created

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>Laravel 9 CRUD Application - ItSolutionStuff.com</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+    <h1>Welcome to index page </h1>
+</body>
+</html>
