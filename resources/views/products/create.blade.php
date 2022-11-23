edd31728b72e5584a78dce5d4103fc1f9eb2a393
Surendra <surendragiri10@gmail.com>
2022-11-23 20:25:00 +0545

    create ui

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 9 CRUD Application - ItSolutionStuff.com</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    
+<div class="container">
+    @yield('content')
+</div>
+    
+@extends('products.layout')
+ 
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Laravel 9 CRUD Example from scratch - ItSolutionStuff.com</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('products.create') }}"> Create New Product</a>
+            </div>
+        </div>
+    </div>
+   
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+   
+    <table class="table table-bordered">
+        <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>Details</th>
+            <th width="280px">Action</th>
+        </tr>
+        @foreach ($products as $product)
+        <tr>
+            <td>{{ ++$i }}</td>
+            <td>{{ $product->name }}</td>
+            <td>{{ $product->detail }}</td>
+            <td>
+                <form action="{{ route('products.destroy',$product->id) }}" method="POST">
+   
+                    <a class="btn btn-info" href="{{ route('products.show',$product->id) }}">Show</a>
+    
+                    <a class="btn btn-primary" href="{{ route('products.edit',$product->id) }}">Edit</a>
+   
+                    @csrf
+                    @method('DELETE')
+      
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+  
+    {!! $products->links() !!}
+      
+@endsection
+
+</body>
+</html>
